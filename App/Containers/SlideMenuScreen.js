16fa06535cd328bcc45e136612a88f1bb9eb21a5
mohamedhamdy2017 <34789872+mohamedhamdy2017@users.noreply.github.com>
2018-11-04 14:43:00 +0200

    Update SlideMenuScreen.js

--- App/Containers/SlideMenuScreen.js
+++ App/Containers/SlideMenuScreen.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import { TouchableOpacity, Image, Text } from 'react-native'
+import { TouchableOpacity, Image, Text, View } from 'react-native'
 import { connect } from 'react-redux'
-import { Container,Content, Icon, Button, Header, Left, Title, Body } from 'native-base'
+import { Container,Content, Header, Right, Left, Title, Body } from 'native-base'
+import { Switch }  from 'react-native-base-switch';
 import images from '../Themes/Images'
 
 
@@ -14,22 +15,55 @@ import styles from './Styles/SlideMenuScreenStyle'
 class SlideMenuScreen extends Component {
   render () {
     return (
-      <Container>
-      <Header>
-        <Left style={{flex:1}}>
+      <Container style={{backgroundColor: '#0c3b87', flex:1}}>
+      <Header style={{backgroundColor: '#0c3b87'}}>
+
+        <Left style={{alignItems: 'center', flex: 1}}>
+            <Title>Wallet</Title>
+        </Left>
+
+        <Right >
           <TouchableOpacity
-           onPress = {() => this.props.navigation.navigate('DrawerOpen')}
+           onPress = {() => this.props.navigation.navigate('DrawerClose')}
           >
           <Image
             source = {images.drawer}
             style={{width:20, height:20, justifyContent:'flex-start'}}
           />
           </TouchableOpacity>
-        </Left>
+        </Right>
+
+        
+      
       </Header>
-      <Content contentContainerStyle={{flex: 1, alignItems: 'center', justifyContent:'center'}}>
-        <Text>SlideScreen</Text>
+      <Content contentContainerStyle={{alignItems: 'flex-start', marginLeft:30}}>
+        <Title style={{color: '#3d92fe', fontSize: 12, marginTop: 30}}>ACCOUNT</Title>
+        <Title style={{color: '#fff', fontSize: 18, marginBottom:10, fontStyle: 'italic'}}>Login</Title>
+        <Title style={{color: '#fff', fontSize: 18, fontStyle: 'italic'}}>Create Account</Title>
+        <Title style={{color: '#3d92fe', fontSize: 12, marginTop: 50}}> SETTINGS </Title>
+        <View style={{alignItems: 'flex-end', flex:1, flexDirection: 'row', marginBottom: 20}}>
+          <Text style={{color: '#fff', fontSize: 18, fontStyle: 'italic', marginRight: 100}}>Currency</Text>
+          <Text style={{color: '#15b9dd', fontSize: 18, marginRight: 130, fontStyle: 'italic'}}>USD -$</Text>
+        </View>
+        <Title style={{color: '#fff', fontSize: 18, marginBottom: 20, fontStyle: 'italic'}}>Categories </Title>
+        <View style={{alignItems: 'flex-end', flex:1, flexDirection: 'row', marginBottom: 20}}>
+          <Text style={{color: '#fff', fontSize: 18, fontStyle: 'italic'}}>Start of Month</Text>
+          <Text style={{color: '#fff', fontSize: 18, fontStyle: 'italic'}}>Sounds</Text>
+        </View>
+        <View style={{alignItems: 'flex-end', flex:1, flexDirection: 'row', marginBottom: 20}}>
+          <Text style={{color: '#fff', fontSize: 18, marginRight: 130, fontStyle: 'italic'}}>Sounds</Text>
+          <Switch  />
+        </View>
+        <View style={{alignItems: 'flex-end', flex:1, flexDirection: 'row', marginBottom: 20}}>
+          <Text style={{color: '#fff', fontSize: 18, marginRight: 75, fontStyle: 'italic'}}>Passcode lock</Text>
+          <Switch />
+        </View>
+        <Title style={{color: '#3d92fe', fontSize: 12, marginTop: 30}}>DATA</Title>
+        <Title style={{color: '#fff', fontSize: 18, marginBottom:10, fontStyle: 'italic'}}>Backup</Title>
+        <Title style={{color: '#fff', fontSize: 18, fontStyle: 'italic'}}>Restore Backup</Title>
+        <Title style={{color: 'red', fontSize: 18, marginVertical: 150, fontStyle: 'italic'}}>Reset All Records</Title>
       </Content>
+
   </Container>
     )
   }
